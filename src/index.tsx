272ae502b4c1ded1e3bb2d616baa0b0f1cbc2374
Andrey Mityagov <amityagov@gmail.com>
2020-12-26 00:52:29 +0300

    some materials

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Switch, Route, useParams } from 'react-router-dom';
 
+import { Button, Typography, Card, Container, makeStyles, CardContent, CardActions, AppBar, Toolbar, IconButton } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  title2: {
+    fontSize: 24,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 const Home = () => {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
   return <div>
-    Home
-    <div>
-      <NavLink to="/verify">
-        Verify
-      </NavLink>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5">
+          ✍message
+        </Typography>
+      </Toolbar>
+    </AppBar>
+
+    <Container maxWidth="lg">
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Word of the Day
+        </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+        </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+        </Typography>
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+          <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Container>
   </div>;
 };
 
